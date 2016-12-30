0a062128ddfe0609afb56910b5d0c28846c7a942
Paul WB <higgsbison@gmail.com>
2016-12-29 16:24:32 -0800

    add new post page and link on index

---
+++ src/components/posts_index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
+
+class PostsIndex extends Component {
+  componentWillMount(){
+    this.props.fetchPosts();
+  }
+  render() {
+    return (
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+          Add a post
+          </Link>
+        </div>
+        List of Blog Posts
+      </div>
+    )
+  }
+}
+
+export default connect(null, { fetchPosts })(PostsIndex);
---
+++ src/reducers/posts.js
@@ -0,0 +1,15 @@
+import { FETCH_POSTS } from '../actions/index';
+
+const INITAL_STATE = {
+  all: [],
+  post: null,
+};
+
+export default function (state = INITAL_STATE, action) {
+  switch(action.type) {
+    case FETCH_POSTS:
+      return { ...state, all: action.payload.data }
+    default:
+      return state;
+  }
+}
